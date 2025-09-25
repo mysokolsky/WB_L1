d757bc84687d61a4b94f21d9562d41c91dacfb13
Michael Sokolskiy <mysokolsky@gmail.com>
2025-09-25 17:13:06 +0300

    === L1.1 == master === ДАТА 25-09-25 === ВРЕМЯ 17:13:06 =====

--- L1.1/main.go
+++ L1.1/main.go
@@ -11,15 +11,19 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	// "github.com/davecgh/go-spew/spew"
 	"github.com/kr/pretty"
 	"github.com/pkg/errors"
 	"math/rand/v2"
 	"os"
-	// "os"
 )
 
-// Superhuman features
+//////////////////////////////////////////////////////////////
+//
+//                 Глобальные  параметры
+//
+//////////////////////////////////////////////////////////////
+
+// Массив сверхспособностей
 var SuperHumanFeatures = [...]string{
 	"Fly",
 	"Fire",
@@ -29,7 +33,7 @@ var SuperHumanFeatures = [...]string{
 	"BulletProof",
 }
 
-// Professions
+// Массив навыков профессий
 var Professions = [...]string{
 	"Economist",
 	"Developer",
@@ -39,6 +43,7 @@ var Professions = [...]string{
 	"Manager",
 }
 
+// Родительская структура
 type Human struct {
 	Name       string  // Имя
 	Age        uint    // возраст
@@ -46,42 +51,68 @@ type Human struct {
 	Parent     *Human  // родитель
 	Children   []Human // дети
 	Partner    *Human  // супруг
-	Profession string  // профессия
+	profession string  // профессия
 }
 
+// Структура, которая полностью наследует поля и методы структуры Human и добавляется ещё одно поле
 type SuperHuman struct {
 	Human
-	SuperFeature string
+	superFeature string
 }
 
+// Структура, которая полностью наследует поля и методы структуры SuperHuman
+type Action struct {
+	SuperHuman
+}
+
+//////////////////////////////////////////////////////////////
+//
+//                    Методы  и  функции
+//
+//////////////////////////////////////////////////////////////
+
+// Появление нового персонажа Human
 func NewHuman(name string, age uint, male bool) *Human {
-	return &Human{
+	human := &Human{
 		Name: name,
 		Age:  age,
 		Male: male,
 	}
+	fmt.Println("Появился новый персонаж:", human.Name, human.Age, "лет")
+	// human.Print()
+	return human
 }
 
+// Появление нового персонажа типа SuperHuman с потенциалом обретения сверхспособности
 func NewSuperHuman(name string, age uint, male bool) *SuperHuman {
 	human := NewHuman(name, age, male)
 	fmt.Println("Появление нового сверхчеловека!")
 	return &SuperHuman{Human: *human}
 }
 
+// Вывод инфо о персонаже
 func prettyPrint(obj interface{}) {
 	b, err := json.MarshalIndent(obj, "", "  ")
 	if err != nil {
-		pretty.Print(obj)
+		pretty.Println(obj) // если ошибка конверсии в json, то выводим через стороннюю библиотеку
 		return
 	}
 	fmt.Println(string(b))
 }
 
+/////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                     //
+//                              М Е Т О Д Ы   H u m a n                                //
+//                                                                                     //
+/////////////////////////////////////////////////////////////////////////////////////////
+
+// Вывод инфо о персонаже
 func (h *Human) Print() {
 	fmt.Println("Вывод информации о персонаже:")
 	prettyPrint(h)
 }
 
+// Процедура бракосочетания
 func (h *Human) Marriage(partner *Human) {
 	var err error
 	if h == nil || partner == nil {
@@ -95,7 +126,9 @@ func (h *Human) Marriage(partner *Human) {
 	} else if h.Partner != nil || partner.Partner != nil {
 		err = errors.New("Бракосочетание пока невозможно! Один или оба партнёра сначала должны развестись.")
 	} else if h.Parent != nil && h.Parent == partner.Parent {
-		err = errors.New("Бракосочетание между родными братом и сестрой невозможно!")
+		err = errors.New("Бракосочетание между родными: братом и сестрой невозможно!")
+	} else if h.Parent != nil && h.Parent == partner {
+		err = errors.New("Бракосочетание между родными: матерью и сыном невозможно!")
 	}
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Ошибка: %+v\n", err)
@@ -108,6 +141,7 @@ func (h *Human) Marriage(partner *Human) {
 	fmt.Printf("%v %v лет и %v %v лет теперь официально семья!\n", h.Name, h.Age, partner.Name, partner.Age)
 }
 
+// Рождение нового персонажа Human
 func (h *Human) ChildrenBorn(name string, male bool) *Human {
 
 	var err error
@@ -133,6 +167,7 @@ func (h *Human) ChildrenBorn(name string, male bool) *Human {
 	return newChild
 }
 
+// Окончание -а для женского рода обращений
 func (h *Human) getEndVerb() string {
 	if h.Male {
 		return ""
@@ -140,16 +175,13 @@ func (h *Human) getEndVerb() string {
 	return "а"
 }
 
+// Получение навыка профессии
 func (h *Human) GetProfession() {
-	h.Profession = Professions[rand.IntN(len(Professions)-1)]
-	fmt.Println("Персонаж", h.Name, "получил"+h.getEndVerb(), "профессию:", h.Profession)
+	h.profession = Professions[rand.IntN(len(Professions)-1)]
+	fmt.Println("Персонаж", h.Name, "получил"+h.getEndVerb(), "профессию:", h.profession)
 }
 
-func (sh *SuperHuman) GetSuperFeature() {
-	sh.SuperFeature = SuperHumanFeatures[rand.IntN(len(SuperHumanFeatures)-1)]
-	fmt.Println("Персонаж", sh.Name, "обрел"+sh.getEndVerb(), "сверхспособность:", sh.SuperFeature)
-}
-
+// Трансформация в сверхчеловека SuperHuman
 func (h *Human) HumanBecomeSuperHuman() *SuperHuman {
 	fmt.Println("C", h.Name, "произошло невероятное событие, изменившее физические возможности!")
 	fmt.Println(h.Name, "стал"+h.getEndVerb(), "сверхчеловеком!")
@@ -158,6 +190,7 @@ func (h *Human) HumanBecomeSuperHuman() *SuperHuman {
 	return newSuperHuman
 }
 
+// Изменение возраста персонажа (и его супруга, если есть)
 func (h *Human) UpdateAge(newAge uint) {
 	oldAge := h.Age
 	h.Age = newAge
@@ -171,9 +204,25 @@ func (h *Human) UpdateAge(newAge uint) {
 	}
 }
 
-type Action struct {
-	SuperHuman
+////////////////////////////////////////////////////////
+//                                                    //
+//           М Е Т О Д Ы   S u p e r H u m a n        //
+//                                                    //
+////////////////////////////////////////////////////////
+
+// Приобретение конкретной сверхспособности
+func (sh *SuperHuman) GetSuperFeature() {
+	sh.superFeature = SuperHumanFeatures[rand.IntN(len(SuperHumanFeatures)-1)]
+	fmt.Println("Персонаж", sh.Name, "обрел"+sh.getEndVerb(), "сверхспособность:", sh.superFeature)
 }
+
+//
+//   ||
+//   ||
+//   ||   ТОЧКА ВХОДА В ПРОГРАММУ
+//   ||
+//  \  /
+//   \/
 
 func main() {
 
@@ -191,16 +240,25 @@ func main() {
 
 	Peter := Vika.ChildrenBorn("Peter", true)
 
+	Peter.UpdateAge(18)
+
 	Jessy := Vika.ChildrenBorn("Jessy", false)
 
 	Vika.Print()
 
 	Jessy.UpdateAge(19)
-	Peter.UpdateAge(18)
+
+	// Jessy.Marriage(Peter)
+
+	// Mika := Jessy.ChildrenBorn("Mika", false)
+
+	// Mika.Print()
 
 	SuperPeter := Peter.HumanBecomeSuperHuman()
 
 	SuperPeter.GetSuperFeature()
+
+	// РЕАЛИЗАЦИЯ ЗАДАЧИ: для объекта типа Action вызываются методы родительской(даже бабушко-дедушкинской) структуры Human
 
 	action := Action{*SuperPeter}
 
