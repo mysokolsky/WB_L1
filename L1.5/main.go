c9606f7dc93ca05cc2275347ade5d09a082a55c2
Michael Sokolskiy <mysokolsky@gmail.com>
2025-10-07 18:54:22 +0300

    === L1.5 == master === ДАТА 07-10-25 === ВРЕМЯ 18:54:22 =====

--- L1.5/main.go
+++ L1.5/main.go
@@ -12,6 +12,45 @@
 
 package main
 
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
 func main() {
+
+	ch := make(chan int, 3)
+
+	var wg sync.WaitGroup
+
+	numWorkers := 5
+	wg.Add(numWorkers)
+	for i := 0; i < numWorkers; i++ {
+
+		go func() {
+			defer wg.Done()
+			for {
+				v, ok := <-ch
+				if ok {
+					fmt.Println(v)
+				} else {
+					return
+				}
+			}
+		}()
+
+	}
+
+	i := 0
+	for {
+		i++
+		select {
+		case <-time.After(3 * time.Second):
+			close(ch)
+			return
+		case ch <- i:
+		}
+	}
 
 }
