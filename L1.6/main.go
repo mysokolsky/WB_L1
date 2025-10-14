3ab1948ea82966ac18c571afa72a82447eba9c88
Michael Sokolskiy <mysokolsky@gmail.com>
2025-10-14 09:31:07 +0300

    === L1.6 == master === ДАТА 14-10-25 === ВРЕМЯ 09:31:07 =====

--- L1.6/main.go
+++ L1.6/main.go
@@ -26,66 +26,83 @@ import (
 
 var wg sync.WaitGroup
 
+// горутина №1
 func autoReturn(name string) {
 	defer wg.Done()
 
 	fmt.Printf("Горутина №1 (%s) завершится через 4 секунды\n", name)
+	defer fmt.Printf("Горутина №1 (%s) завершила работу\n", name)
+
+	fmt.Println("g1: работа горутины...") // безканальная работа
 	time.Sleep(4000 * time.Millisecond)
-	fmt.Printf("Горутина №1 (%s) завершает работу\n", name)
+	fmt.Println("Горутина №1: авто выход по завершению работы функции")
+
 }
 
+// горутина №2
 func closeCh(name string, ch2 chan int) {
 	defer wg.Done()
 
 	fmt.Printf("Горутина №2 (%s) завершится через 4 секунды\n", name)
-	for value := range ch2 {
+	defer fmt.Printf("Горутина №2 (%s) завершила работу\n", name)
+
+	for value := range ch2 { // читаем данные из канала
 		fmt.Println("g2:", value)
-	}
-	fmt.Printf("Горутина №2 (%s) завершает работу\n", name)
+	} // условие завершения горутины
+
+	fmt.Println("Горутина №2: выход по закрытию канала")
+
 }
 
+// горутина №3
 func signalCh(name string, ch3 chan int, quitCh chan struct{}) {
 	defer wg.Done()
 
 	fmt.Printf("Горутина №3 (%s) завершится через 4 секунды\n", name)
+	defer fmt.Printf("Горутина №3 (%s) завершила работу\n", name)
+
 	for {
 		select {
-		case value := <-ch3:
+		case value := <-ch3: // читаем данные из канала
 			fmt.Println("g3:", value)
-		case <-quitCh:
+		case <-quitCh: // условие завершения горутины
+			fmt.Println("Горутина №3: выход по закрытию сигнального канала")
 			close(ch3)
-			fmt.Printf("Горутина №3 (%s) завершает работу\n", name)
 			return
 		}
 	}
 
 }
 
+// горутина №4
 func contextExit(name string, ch4 chan int, ctx context.Context) {
 	defer wg.Done()
+
 	fmt.Printf("Горутина №4 (%s) завершится через 4 секунды\n", name)
-	defer fmt.Printf("Горутина №4 (%s) завершает работу\n", name)
+	defer fmt.Printf("Горутина №4 (%s) завершила работу\n", name)
 
 	for {
 		select {
-		case <-ctx.Done():
-			fmt.Println("Получен сигнал отмены по контексту")
+		case <-ctx.Done(): // условие завершения горутины
+			fmt.Println("Горутина №4: выход по сигналу отмены контекста")
 			return
-		case value := <-ch4:
+		case value := <-ch4: // читаем данные из канала
 			fmt.Println("g4:", value)
 		}
 	}
 }
 
+// горутина №5
 func timerExit(name string, ch5 chan int) {
 	defer wg.Done()
+
 	fmt.Printf("Горутина №5 (%s) завершится через 4 секунды\n", name)
-	defer fmt.Printf("Горутина №5 (%s) завершает работу\n", name)
+	defer fmt.Printf("Горутина №5 (%s) завершила работу\n", name)
 
 	for {
-
-		value, ok := <-ch5
-		if !ok {
+		value, ok := <-ch5 // читаем данные из канала
+		if !ok {           // условие завершения горутины
+			fmt.Println("Горутина №5: выход по окончанию таймера")
 			return
 		}
 		fmt.Println("g5:", value)
@@ -93,41 +110,50 @@ func timerExit(name string, ch5 chan int) {
 
 }
 
-func main() {
-
+// вызов горутины №1
+func autoReturnRun() {
 	wg.Add(1)
-	go autoReturn("автовыход через Return")
+	go autoReturn("автовыход через Return") // запуск горутины
+}
 
+// вызов горутины №2
+func closeChRun() {
 	ch2 := make(chan int)
 	wg.Add(1)
-	go closeCh("закрытие канала", ch2)
+	go closeCh("закрытие канала", ch2) // запуск горутины
 	for i := 0; ; i++ {
-		ch2 <- i
+		ch2 <- i // наполнение канала данными
 		time.Sleep(400 * time.Millisecond)
 		if i > 10 {
 			close(ch2)
 			break
 		}
 	}
+}
 
+// вызов горутины №3
+func signalChRun() {
 	ch3 := make(chan int)
 	quitCh := make(chan struct{})
 	wg.Add(1)
-	go signalCh("сигнальный канал", ch3, quitCh)
+	go signalCh("сигнальный канал", ch3, quitCh) // запуск горутины
 	for i := 0; ; i++ {
-		ch3 <- i * i
+		ch3 <- i * i // наполнение канала данными
 		time.Sleep(400 * time.Millisecond)
 		if i > 10 {
 			close(quitCh) // закрываем сигнальный канал
 			break
 		}
 	}
+}
 
+// вызов горутины №4
+func contextExitRun() {
 	ch4 := make(chan int)
-	ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(4*time.Second))
+	ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(4*time.Second)) // завершаем контекст при достижении момента времени "сейчас + 4 секунды"
 	defer cancel()
 	wg.Add(1)
-	go contextExit("context", ch4, ctx)
+	go contextExit("context", ch4, ctx) // запуск горутины
 loop:
 	for i := 0; i < 5; i++ {
 		select {
@@ -135,27 +161,39 @@ loop:
 			close(ch4)
 			break loop
 		default:
-			ch4 <- i*i + i
+			ch4 <- i*i + i // наполнение канала данными
 			time.Sleep(400 * time.Millisecond)
 		}
 
 	}
+}
 
+// вызов горутины №5
+func timerExitRun() {
 	ch5 := make(chan int)
 	timer := time.After(4 * time.Second)
 	wg.Add(1)
-	go timerExit("time.After", ch5)
+	go timerExit("time.After", ch5) // запуск горутины
 loop2:
 	for i := 0; ; i++ {
 		select {
-		case <-timer: // при закрытии канала
-			close(ch5) // закрываем канал
+		case <-timer: // при закрытии канала таймера
+			close(ch5) // закрываем канал данных
 			break loop2
 		default:
-			ch5 <- i*i - i
+			ch5 <- i*i - i // наполнение канала данными
 			time.Sleep(400 * time.Millisecond)
 		}
 	}
+}
+
+func main() {
+
+	autoReturnRun()  // 1 - автовыход через Return
+	closeChRun()     // 2 - закрытие канала
+	signalChRun()    // 3 - сигнальный канал
+	contextExitRun() // 4 - context с дедлайном
+	timerExitRun()   // 5 - time.After
 
 	wg.Wait()
 }
