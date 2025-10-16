5cc445fe3bc75ee893505d428d15888543e5be57
Michael Sokolskiy <mysokolsky@gmail.com>
2025-10-16 16:18:54 +0300

    === L1.6 == master === ДАТА 16-10-25 === ВРЕМЯ 16:18:54 =====

--- L1.6/main.go
+++ L1.6/main.go
@@ -20,11 +20,11 @@ package main
 import (
 	// "context"
 	"fmt"
-	"os"
-	"os/signal"
+	// "os"
+	// "os/signal"
 	"runtime"
 	"sync"
-	"syscall"
+	// "syscall"
 	"time"
 )
 
@@ -114,17 +114,38 @@ var wg sync.WaitGroup
 
 // }
 
-// горутина №6
-func osInterrupt(name string, ch6 chan int) {
+// // горутина №6
+// func osInterrupt(name string, ch6 chan int) {
+// 	defer wg.Done()
+
+// 	fmt.Printf("Горутина №6 (%s) завершится через 4 секунды\n", name)
+// 	defer fmt.Printf("Горутина №6 (%s) завершила работу\n", name)
+
+// 	for value := range ch6 { // читаем данные из канала
+// 		fmt.Println("g6:", value)
+// 	}
+// 	// fmt.Println("\nГорутина №6: выход по сигналу ОС")
+// }
+
+// горутина №7
+func runtimeExit(name string, ch7 chan failObj) {
+
 	defer wg.Done()
 
-	fmt.Printf("Горутина №6 (%s) завершится через 4 секунды\n", name)
-	defer fmt.Printf("Горутина №6 (%s) завершила работу\n", name)
+	fmt.Printf("Горутина №7 (%s) завершится через 2 секунды\n", name)
+	defer fmt.Printf("Горутина №7 (%s) завершила работу\n", name)
 
-	for value := range ch6 { // читаем данные из канала
-		fmt.Println("g6:", value)
-	}
-	// fmt.Println("\nГорутина №6: выход по сигналу ОС")
+	defer runtime.Goexit() // вызываем принудительное завершение горутины. Паники автоматически сбрасываются и продолжается вызов отложенных функций.
+
+	defer func() { s := ""; print(s[1]) }()         // получаем доступ к несуществующему элементу, вызывая панику
+	defer func() { close(ch7); ch7 <- failObj{} }() // пытаемся записать объект в закрытый канал. Возникает ещё одна паника
+	// defer                    // закрываем канал
+
+	time.Sleep(2 * time.Second)
+	value := <-ch7            // читаем объект из канала
+	fmt.Println(*(value.obj)) // паника от разыменования nil-указателя вызывает принудительный выход из функции
+
+	fmt.Println("эта строчка никогда не будет достигнута из-за паники выше")
 }
 
 // // вызов горутины №1
@@ -204,60 +225,41 @@ func osInterrupt(name string, ch6 chan int) {
 // 	}
 // }
 
-// вызов горутины №6
-func osInterruptRun() {
-	ch6 := make(chan int)
-	timer := time.After(5 * time.Second) // если не нажимать Ctrl + C, то завершаем по таймеру через 5 секунд
+// // вызов горутины №6
+// func osInterruptRun() {
+// 	ch6 := make(chan int)
+// 	timer := time.After(5 * time.Second) // если не нажимать Ctrl + C, то завершаем по таймеру через 5 секунд
 
-	interruptCh := make(chan os.Signal, 1)
-	signal.Notify(interruptCh, os.Interrupt, syscall.SIGTERM) // слушаем системные сигналы Ctrl + C и kill pid
-	fmt.Println("для выхода из горутины нажмите Ctrl + C")
-	wg.Add(1)
-	go osInterrupt("Ctrl + C, kill pid", ch6) // запуск горутины
+// 	interruptCh := make(chan os.Signal, 1)
+// 	signal.Notify(interruptCh, os.Interrupt, syscall.SIGTERM) // слушаем системные сигналы Ctrl + C и kill pid
+// 	fmt.Println("для выхода из горутины нажмите Ctrl + C")
+// 	wg.Add(1)
+// 	go osInterrupt("Ctrl + C, kill pid", ch6) // запуск горутины
 
-loop3:
-	for i := 0; ; i++ {
-		select {
-		case <-interruptCh: // при получении сигнала системы и закрытии канала
-			fmt.Println("\nГорутина №6: завершение по сигналу пользователя или ОС")
-			close(ch6) // закрываем канал данных
-			break loop3
-		case <-timer:
-			fmt.Print("\nГорутина №6: завершение по таймеру")
-			close(ch6) // закрываем канал данных
-			break loop3
-		default:
-			ch6 <- i*i - i + 2*i // наполнение канала данными
-			time.Sleep(1 * time.Second)
-		}
-	}
+// loop3:
+// 	for i := 0; ; i++ {
+// 		select {
+// 		case <-interruptCh: // при получении сигнала системы и закрытии канала
+// 			fmt.Println("\nГорутина №6: завершение по сигналу пользователя или ОС")
+// 			close(ch6) // закрываем канал данных
+// 			break loop3
+// 		case <-timer:
+// 			fmt.Print("\nГорутина №6: завершение по таймеру")
+// 			close(ch6) // закрываем канал данных
+// 			break loop3
+// 		default:
+// 			ch6 <- i*i - i + 2*i // наполнение канала данными
+// 			time.Sleep(1 * time.Second)
+// 		}
+// 	}
 
-	signal.Stop(interruptCh) // прекращаем отслеживать сигнал пользователя на Ctrl + C и kill pid
-	close(interruptCh)       // закрываем специальный канал
+// 	signal.Stop(interruptCh) // прекращаем отслеживать сигнал пользователя на Ctrl + C и kill pid
+// 	close(interruptCh)       // закрываем специальный канал
 
-}
+// }
 
 type failObj struct {
 	obj *string
-}
-
-func runtimeExit(name string, ch7 chan failObj) {
-
-	defer wg.Done()
-
-	fmt.Printf("Горутина №7 (%s) завершится через 2 секунды\n", name)
-	defer fmt.Printf("Горутина №7 (%s) завершила работу\n", name)
-
-	defer runtime.Goexit() // вызываем принудительное завершение горутины. Паники автоматически сбрасываются и продолжается вызов отложенных функций.
-
-	defer func() { ch7 <- failObj{} }() // пытаемся записать объект в закрытый канал. Возникает ещё одна паника
-	defer close(ch7)                    // закрываем канал
-
-	time.Sleep(2 * time.Second)
-	value := <-ch7            // читаем объект из канала
-	fmt.Println(*(value.obj)) // паника от разыменования nil-указателя вызывает принудительный выход из функции
-
-	fmt.Println("эта строчка никогда не будет достигнута из-за паники выше")
 }
 
 // вызов горутины №7
@@ -277,7 +279,7 @@ func main() {
 	// signalChRun()    // 3 - сигнальный канал
 	// contextExitRun() // 4 - context с дедлайном
 	// timerExitRun()   // 5 - time.After
-	osInterruptRun() // 6 - Ctrl + C, kill pid...
+	// osInterruptRun() // 6 - Ctrl + C, kill pid...
 	runtimeExitRun() // 7 - runtime.Goexit
 
 	wg.Wait()
