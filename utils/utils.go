a5b13aed96361544396f1b73082d2e5163779251
Michael Sokolskiy <mysokolsky@gmail.com>
2025-11-18 11:17:12 +0300

    update utils

--- utils/utils.go
+++ utils/utils.go
@@ -39,6 +39,9 @@ func QuickSort(mas []int, left, right int) error {
 
 	defer func() {
 		fmt.Printf("\n---------- Выход из цикла рекурсии %v -----------\n\n", counter)
+		if counter == 1 {
+			println("\nРяд чисел отсортирован.")
+		}
 		counter--
 	}()
 	counter++
@@ -106,6 +109,7 @@ func QuickSort(mas []int, left, right int) error {
 		fmt.Printf("Правый подмассив для сортировки:\n%+v\n", mas[i:right+1])
 		QuickSort(mas, i, right)
 	}
+
 	return nil
 }
 
@@ -113,36 +117,41 @@ func QuickSort(mas []int, left, right int) error {
 // На вход принимает указатель на слайс mas, служебную переменную offset первого индекса ряда (обычно это 0), и искомое число target
 // На выход отправляет индекс найденного числа (первого попавшегося если такое число в слайсе не одно, а не первого от начала слайса) или -1 если число не нашлось
 func BinSearch(mas []int, target int) int {
+	println("\n===============================================================")
+	println("\nПоиск числа", target, "(target) в сортированном ряде чисел.")
 
 	left := 0
 	right := len(mas) - 1
 
-	if right-left == 0 {
-		if mas[0] == target {
-			return 0
-		}
-	}
+	iter := 0
 
 	for {
+
+		iter++
+
+		fmt.Printf("\n ---- Итерация №%v ---- \n", iter)
 		// если target не найден, то:
-		if right-left <= 0 {
+		if left > right {
 			return -1
 		}
 
 		middle := (right + left) / 2
-		offset := (right - left) / 2
 
+		println("Ряд чисел:")
 		fmt.Printf("%+v\n", mas[left:right+1])
-		println("Индекс среднего:", middle)
-		println("Левый край left =", left)
-		println("Правый край right =", right)
+		println("Индекс левой границы left =", left)
+		println("Индекс правой границы right =", right)
+		println("Индекс среднего элемента middle:", middle)
 
 		if mas[middle] == target {
+			fmt.Printf("middle = target: %v = %v \n\n", mas[middle], target)
 			return middle
 		} else if mas[middle] < target {
-			left += offset + 1
+			left = middle + 1
+			fmt.Printf("middle < target: %v < %v \n", mas[middle], target)
 		} else {
-			right -= offset
+			right = middle - 1
+			fmt.Printf("middle > target: %v > %v \n", mas[middle], target)
 		}
 	}
 
