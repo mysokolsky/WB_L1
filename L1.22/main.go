a793f29b246a6415335649668e82fed8c5f3b35e
Michael Sokolskiy <mysokolsky@gmail.com>
2025-12-06 19:20:46 +0300

    === L1.23 == master === ДАТА 06-12-25 === ВРЕМЯ 19:20:45 =====

--- L1.22/main.go
+++ L1.22/main.go
@@ -12,6 +12,28 @@
 
 package main
 
-func main() {
+import (
+	"fmt"
+	"math/big"
+)
 
+type BigNum struct {
+}
+
+// func NumToString(num interface{}) string{
+
+// }
+
+func Add(num1, num2 interface{}) string {
+	var result string
+
+	return result
+}
+
+func main() {
+	f1 := big.NewFloat(0)
+	f1.SetString("3.14159265358979323846") // High-precision pi
+	f2 := big.NewFloat(2)
+	result := new(big.Float).Mul(f1, f2)
+	fmt.Println(result)
 }
